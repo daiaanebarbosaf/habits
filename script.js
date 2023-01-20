565d8e77d7caf61775b846dba53fa79686863dd1
Daiane Farias <daiaanebarbosaf@gmail.com>
2023-01-20 14:46:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ const nlwSetup = new NLWSetup(form)
 
 const button = document.querySelector('header button')
 
+button.addEventListener('click', add)
+
+function add() {
+  alert('Hello Word!')
+}
+
 /*const data = {
   run: ['01-01', '01-02', '01-06', '01-07', '01-08'],
   takePills:['01-03'],
