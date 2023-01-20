95b532c7f047aed23dc6c3ac9fd58111240488be
Daiane Farias <daiaanebarbosaf@gmail.com>
2023-01-20 14:19:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const nlwSetup = new NLWSetup(form)
 
 const data = {
   run: ['01-01', '01-02', '01-06'],
-  takePills:['01-03']
+  takePills:['01-03'],
+  journal:['01-02']
 }
 
 nlwSetup.setData(data)
