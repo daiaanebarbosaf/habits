b003e6d185c6e40a47672d845e3f937d539b91fb
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-01-21 13:56:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const nlwSetup = new NLWSetup(form)
 
 const button = document.querySelector('header button')
 
-button.addEventListener('click', add)
+button.addEventListener("click", add)
+form.addEventListener("chage", save)
 
 function add() {
   const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
@@ -15,6 +16,10 @@ function add() {
   }
   alert('Dia adicionado com sucesso!✔')
   nlwSetup.addDay(today)
+}
+
+function save(){
+  
 }
 
 /*const data = {
